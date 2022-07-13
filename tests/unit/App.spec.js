d30beaea284f653f7ca7e0ce429294d5503df8f8
Yevhen <shershen.yevhen@gmai>
2022-07-13 19:37:22 +0200

    $change when $buttonText button clicked

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,20 +1,18 @@
 import App from "@/App.vue";
 import { mount } from "@vue/test-utils";
-import { nextTick } from "vue";
+
 
 describe("Counter", () => {
   let wrapper;
-  const findPlusButton = () =>
-    wrapper.findAll("button").wrappers.find((w) => w.text() === "+");
-
-    afterEach(() => {
-    wrapper.destroy();
-  });
+  const findButtonByText = (text) =>
+    wrapper.findAll("button").wrappers.find((w) => w.text() === text);
 
   const createComponent = () => {
     wrapper = mount(App);
   };
-
+  afterEach(() => {
+    wrapper.destroy();
+  });
   it("shows 0 when initialized", () => {
     //Arrange
     createComponent();
@@ -25,13 +23,21 @@ describe("Counter", () => {
     //не интересует в данном тесте гда находится элемент, а интересует только его инициализирование
     expect(wrapper.text()).toContain("0");
   });
+  //https://jestjs.io/docs/api#testeachtablename-fn-timeout
+  it.each`
+    buttonText | change                 | expectedResult
+    ${"+"}     | ${"increments by one"} | ${"1"}
+    ${"-"}     | ${"decrements by one"} | ${"-1"}
+  `(
+    "$change when $buttonText button clicked",
+    async ({ buttonText, expectedResult }) => {
+      createComponent();
 
-  it("increments by one when + button clicked", async ()=>{
-    createComponent();
-    await findPlusButton().trigger('click');
-    //await nextTick() - подождать пока произойдет следующий тик
-    //nextTick - это Утилита для ожидания следующего сброса обновления DOM.
-    // await nextTick();
-    expect(wrapper.text()).toContain('1');
-  });
+      await findButtonByText(buttonText).trigger("click");
+      //await nextTick() - подождать пока произойдет следующий тик
+      //nextTick - это Утилита для ожидания следующего сброса обновления DOM.
+      // await nextTick();
+      expect(wrapper.text()).toContain(expectedResult);
+    }
+  );
 });
