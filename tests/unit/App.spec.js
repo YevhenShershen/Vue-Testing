c7c6d4e78f44d3c3368b8a1d02eaf8a6762e623a
Yevhen <shershen.yevhen@gmai>
2022-07-13 08:26:31 +0200

    increments by one when + button clicked

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,25 +1,37 @@
 import App from "@/App.vue";
-import {mount} from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
+import { nextTick } from "vue";
 
-describe('Counter', ()=>{
+describe("Counter", () => {
+  let wrapper;
+  const findPlusButton = () =>
+    wrapper.findAll("button").wrappers.find((w) => w.text() === "+");
 
-let wrapper;
-afterEach(()=>{
-  wrapper.destroy();
+    afterEach(() => {
+    wrapper.destroy();
+  });
+
+  const createComponent = () => {
+    wrapper = mount(App);
+  };
+
+  it("shows 0 when initialized", () => {
+    //Arrange
+    createComponent();
+    //Act
+
+    //Assert
+    //expect(wrapper.find("#app")text()).toContain('0') - такая запись не приемлемая так как нас
+    //не интересует в данном тесте гда находится элемент, а интересует только его инициализирование
+    expect(wrapper.text()).toContain("0");
+  });
+
+  it("increments by one when + button clicked", async ()=>{
+    createComponent();
+    await findPlusButton().trigger('click');
+    //await nextTick() - подождать пока произойдет следующий тик
+    //nextTick - это Утилита для ожидания следующего сброса обновления DOM.
+    // await nextTick();
+    expect(wrapper.text()).toContain('1');
+  });
 });
-
-const createComponent = () =>{
-  wrapper = mount(App);
-}
-
-  it('shows 0 when initialized',()=>{
-//Arrange
-createComponent();
-//Act
-
-//Assert
-//expect(wrapper.find("#app")text()).toContain('0') - такая запись не приемлемая так как нас
-//не интересует в данном тесте гда находится элемент, а интересует только его инициализирование
-expect(wrapper.text()).toContain('0')
-  })
-})
